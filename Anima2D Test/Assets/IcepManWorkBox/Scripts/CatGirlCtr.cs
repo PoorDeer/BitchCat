7bf886a1bd0416092d2cad9750e349a3273a5525
icepoleman <qqkoko1235@gmail.com>
2020-04-06 00:04:53 +0800

    玩法確定 2D Light加入
    動畫有修改 全脫的動畫改直接關Cloth

---
+++ Anima2D Test/Assets/IcepManWorkBox/Scripts/CatGirlCtr.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatGirlCtr : MonoBehaviour
+{
+    Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+  
+}
+
+public enum CatGirlStatus
+{
+    Idle,
+    Walk,
+    Run
+    
+};
