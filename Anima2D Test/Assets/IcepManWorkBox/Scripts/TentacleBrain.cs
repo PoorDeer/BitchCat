0ccd6f2c1109a51dfab34a44853204c5c2615757
icepoleman <qqkoko1235@gmail.com>
2020-04-29 17:10:55 +0800

    一串數字取幾個不重複

---
+++ Anima2D Test/Assets/IcepManWorkBox/Scripts/TentacleBrain.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TentacleBrain : MonoBehaviour
+{
+    int[] fireOrder = new int[20];
+
+    int fireNum = 5;//攻擊的砲管數
+
+    void Start()
+    {
+        for (int i = 0; i < fireNum; i++)
+        {
+            fireOrder[i] = Random.Range(1, 20);
+
+            for (int j = 0; j < i; j++)
+            {
+                if (fireOrder[j] == fireOrder[i])
+                {
+                    j = 0;
+                    fireOrder[i] = Random.Range(0, 10);
+                }
+            }
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
