1a416b9a1a861fc24f96cb777bd9ed6731733ba3
icepoleman <qqkoko1235@gmail.com>
2020-06-03 07:16:57 +0800

    資料整理

---
+++ Assets/IcepManWorkBox/Scripts/TentacleBrain.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TentacleBrain : MonoBehaviour
+{
+    public static float nextShoot_Time = 3;
+
+    void Start()
+    {
+
+    }
+
+  
+}
---
+++ Assets/IcepManWorkBox/Scripts/TentacleCtr.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TentacleCtr : MonoBehaviour
+{
+    Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        ShootOrder();
+    }
+    void Update()
+    {
+   
+    }
+
+    public void ShootOrder()
+    {
+        float nextShoot_T = Random.Range(TentacleBrain.nextShoot_Time, 8);
+        StartCoroutine(ShootOrder(nextShoot_T));
+    }
+
+    IEnumerator ShootOrder(float toShoot_t)
+    {
+        yield return new WaitForSeconds(toShoot_t);
+        anim.Play("Atk");
+ 
+        yield return new WaitForSeconds(2.5f);
+
+        ShootOrder();
+    }
+}
